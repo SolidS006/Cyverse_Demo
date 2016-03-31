c677388cd8b011834bbf15f95e62dfb8cb4f86b4
Suchi Perera <suchi@email.arizona.edu>
2016-03-31 13:58:49 -0700

    dplyr was installed.

--- Code/0331_R_FancyWay.R
+++ Code/0331_R_FancyWay.R
@@ -6,4 +6,8 @@
 
 gap.in<-read.csv("https://raw.githubusercontent.com/resbaz/r-novice-gapminder-files/master/data/gapminder-FiveYearData.csv")
 
+# install.packages("dplyr",dependencies =TRUE)
+# note that the above was commneted so that it won't run everytime we run the script.
+# it can be rerun in another machine or something later.
 
+# loading the packages to the memmory
