1b46eef4949f742aa1ea53b76b9f13d0ea4e7982
Suchi Perera <suchi@email.arizona.edu>
2016-03-31 13:51:47 -0700

    Initial commit for dply.  done!

---
+++ Code/0331_R_FancyWay.R
@@ -0,0 +1,9 @@
+#well commented code is good code
+#Scrpt to analyze the gapminder dataset using dply
+#March 31st 2016 1351 h
+#Suchi Perera
+#suchi@email.arizona.edu
+
+gap.in<-read.csv("https://raw.githubusercontent.com/resbaz/r-novice-gapminder-files/master/data/gapminder-FiveYearData.csv")
+
+
