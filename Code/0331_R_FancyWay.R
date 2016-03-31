181a127a6a648527b5ebe45a1b5ae7d9c4171261
Suchi Perera <suchi@email.arizona.edu>
2016-03-31 14:56:28 -0700

    Life expectancy quiz.

--- Code/0331_R_FancyWay.R
+++ Code/0331_R_FancyWay.R
@@ -33,3 +33,57 @@ mean(broken.gap.in$pop)
 # we downloaded the file using R,  yo opene dthe dat a file and changes it. Bas ide so tho do it.
 #  Youo can rewritw the csv.  Do not git a file nore than 10 GBs.
 
+# we have acces to pipes now.  
+# in deplyr we have a very similar concept as shell pipes.
+# \\
+
+
+gap.in %>%
+  filter(country=="China")
+
+
+  # deplyr has a a lot of functions we can use more conveniently.
+  # R does not care about the line breaks
+# note that the gap.in%>% and the next line had to be run simultaneously for me to get the desired output
+
+gap.in %>%
+  filter(country=="China") %>%
+  select(year,lifeExp)
+
+
+gap.in %>%
+  filter(country %in% c("China", "Argentina")) %>%
+  select(country,year,lifeExp) %>%
+  group_by(country) %>%
+  summarize(lifeExpMean = mean(lifeExp), LifeExpSD = sd(lifeExp), LifeExpSum = sum(lifeExp))
+
+
+# Calcukate the average life expectancies per country for all countries.  Which country had tge longest life expectncey and whihch had the shortest?
+
+gap.in %>%
+  filter(country %in% c("China", "Argentina", "Sri Lanka")) %>%
+  select(country,year,lifeExp) %>%
+  group_by(country) %>%
+  summarize(lifeExpAvg = mean(lifeExp), sort(lifeExpAvg,decreasing = FALSE))
+
+gap.in %>%
+  group_by(country)%>%
+  summarize(meanLifeExp = mean(lifeExp))
+
+# dat frames All of them cannot be diplayed.  we can do a variable assigne
+
+summarized.gap.in %>%
+  group_by(country)%>%
+  summarize(meanLifeExp = mean(lifeExp))
+
+gap.in %>%
+  group_by(country)%>%
+  summarize(meanLifeExp = mean(lifeExp))->summarized.gap
+  
+gap.in %>%
+  group_by(country)%>%
+  summarize(meanLifeExp = mean(lifeExp)) %>%
+  arrange(desc(meanLifeExp))
+
+->summarized.gap
+
